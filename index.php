55cfae17ab09638dd6145c920e43b8acbef8cc78
Dev-Putra12 <putraagus1204@gmail.com>
2023-10-30 01:34:19 +0800

    update

--- index.php
+++ index.php
@@ -1,8 +1,56 @@
-<?php include 'koneksi.php';
+<?php
+include 'koneksi.php';
 session_start();
-if ($_SESSION['nim'] != true) {
-    header("location:login.php?pesan=belum_login");
+
+// Cek jika session 'nim' ada
+if (!isset($_SESSION['nim'])) {
+    header("Location: login.php?pesan=belum_login");
+    exit;
 }
+
+// PDO (PHP Data Object) initialization 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "akar-kuadrat";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+$nim = $_SESSION['nim'];
+$is_found = false;
+$dataPerhalaman = [];
+$n = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Cek jika 'nilai' ada dalam $_POST
+    $n = isset($_POST['nilai']) ? $_POST['nilai'] : null;
+
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i * $i === $n) {
+            $sql = "INSERT INTO squareroot(nim, input_number, result) VALUES (?, ?, ?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$nim, $n, $i]);
+            $is_found = true;
+            break;
+        }
+    }
+
+    if (!$is_found) {
+        $sql = "INSERT INTO squareroot(nim, input_number, result) VALUES (?, ?, ?)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$nim, $n, 0]);
+    }
+}
+// menampilkan hasil dalam format JSON
+// header('Content-type: application/json');
+// echo json_encode(['success' => true]);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +60,39 @@ if ($_SESSION['nim'] != true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kalkulator Akar Kuadrat</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        function hanyaAngka() {
+            var angka = event.which;
+            if (angka < 48 || angka > 57) {
+                return false;
+            }
+            return true;
+        }
+
+        function hitungAkar() {
+            // Mengambil nilai dari input
+            const nilai = document.getElementById('nilai').value;
+            // Melakukan permintaan ke API
+            fetch(`http://hi-kode.com/index.php?nilai=${nilai}`)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
+
+        function hitungSql() {
+            // Dapatkan angka dari input
+            const nilai = document.getElementById('nilai').value;
+            // Buat request ke API PHP
+            // Perhatikan Anda harus mengganti URL dengan URL API yang benar
+            fetch(`http://hi-kode.com/index.php?nilai=${nilai}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    // Tangani data yang diterima dari API di sini
+                })
+                .catch(error => console.error('Error:', error));
+        }
+        document.querySelector('#hitungSqlButton').addEventListener('click', hitungSql);
+    </script>
 </head>
 
 <body>
@@ -26,12 +107,12 @@ if ($_SESSION['nim'] != true) {
             <!-- form -->
             <form>
                 <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Masukan
+                    <label for="nilai" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Masukan
                         Nilai:</label>
-                    <input type="text" id="nilai" name="nilai" onkeypress="return hanyaAngka(event)" placeholder="Hanya Masukan Angka" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required>
+                    <input type="number" id="nilai" name="nilai" onkeypress="return hanyaAngka(event)" placeholder="Hanya Masukan Angka" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
                 </div>
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="hitungAkar()">Hitung Akar Kuadrat</button>
-                <button type="submit" class="mb-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="hitungAkar()">Hitung PL</button>
+                <button type="submit" class="mb-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="hitungSql()">Hitung PL</button>
             </form>
             <!-- end form -->
 
@@ -42,7 +123,6 @@ if ($_SESSION['nim'] != true) {
             <!-- end hasil -->
 
             <!-- tables -->
-
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-50">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
@@ -51,10 +131,10 @@ if ($_SESSION['nim'] != true) {
                                 ID
                             </th>
                             <th scope="col" class="px-6 py-3">
-                                Number
+                                Input Number
                             </th>
                             <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                Square Root
+                                Result
                                 <!-- ini gatau apa -->
                             </th>
                             <th scope="col" class="px-6 py-3">
@@ -62,37 +142,78 @@ if ($_SESSION['nim'] != true) {
                             </th>
                         </tr>
                     </thead>
+                    <?php
+                    // ambil data berdasarkan session yang login
+                    $nim = $_SESSION['nim'];
+                    $result = mysqli_query($connect, "SELECT * FROM squareroot WHERE nim='$nim'");
+                    $query = "SELECT id,input_number,result,created_at FROM squareroot WHERE nim ='$nim'";
+                    // Pagination
+                    $limitData = 5;
+                    $getTotalData = $pdo->prepare("SELECT * FROM squareroot WHERE nim=?");
+                    $getTotalData->execute([$nim]);
+                    $totalData = $getTotalData->rowCount();
+                    $jumlahPagination = ceil($totalData / $limitData);
+                    $page = isset($_GET["halaman"]) ? $_GET["halaman"] : 1;
+                    $dataAwal = ($page * $jumlahPagination) - $jumlahPagination;
+                    $dataPerhalamanStatement = $pdo->prepare("SELECT * FROM squareroot WHERE nim=? LIMIT ?,?");
+                    $dataPerhalamanStatement->bindParam(1, $nim, PDO::PARAM_STR);
+                    $dataPerhalamanStatement->bindParam(2, $dataAwal, PDO::PARAM_INT);
+                    $dataPerhalamanStatement->bindParam(3, $limitData, PDO::PARAM_INT);
+                    $dataPerhalamanStatement->execute();
+                    $dataPerhalaman = $dataPerhalamanStatement->fetchAll();
+                    // tampilkan data dari tabel user
+                    $user = mysqli_query($connect, $query);
+                    $row = mysqli_fetch_array($user);
+                    ?>
                     <tbody>
-                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Silver
-                            </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Laptop
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
+                        <?php foreach ($dataPerhalaman as $row) : ?>
+                            <tr class="border-b border-gray-200 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                    <?= $row['id'] ?>
+                                </th>
+                                <td class="px-6 py-4">
+                                    <?= $row['input_number'] ?>
+                                </td>
+                                <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                                    <?= $row['result'] ?>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <?= $row['created_at'] ?>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
                     </tbody>
                 </table>
+                <!-- Paginasi -->
+                <nav aria-label="Page navigation example">
+                    <ul class="list-style-none flex">
+                        <?php if ($page > 1) : ?>
+                            <li>
+                                <a class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white" href="?halaman=<?= $page - 1 ?>">Previous</a>
+                            </li>
+                        <?php endif ?>
+                        <?php for ($i = 1; $i <= $jumlahPagination; $i++) : ?>
+                            <?php if ($page == $i) : ?>
+                                <li>
+                                    <a class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-blue-700 dark:hover:text-white" href="?halaman=<?php echo $i ?>"> <?php echo $i ?> </a>
+                                </li>
+                            <?php else : ?>
+                                <li>
+                                    <a class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white" href="?halaman=<?php echo $i ?>"> <?php echo $i ?> </a>
+                                </li>
+                            <?php endif ?>
+                        <?php endfor; ?>
+                        <?php if ($page < $jumlahPagination) : ?>
+                            <li>
+                                <a class="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white" href="?halaman=<?= $page + 1 ?>">Next</a>
+                            </li>
+                        <?php endif ?>
+                    </ul>
+                </nav>
             </div>
             <!-- end tables -->
         </div>
     </section>
-    <!-- form section -->
-    <!-- end tailwind -->
-    <!-- <form action="hasil.php" method="post">
-                    <label for="nilai">Masukkan Nilai:</label>
-                    <input type="text" class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600" id="nilai" name="nilai" onkeypress="return hanyaAngka(event)" placeholder="Hanya Masukan Angka">
-                    <button type="button" onclick="hitungAkar()">Hitung Akar Kuadrat</button>
-                </form>
-                <form action="hasil.php" method="post">
-                    <button type="button" onclick="hitungAkar()">Hitung PL</button>
-                </form> -->
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register & Login Page</title>
+    <title>Login Page</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
@@ -26,13 +26,6 @@
                                     <label for="nim" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Masukan
                                         Nim</label>
                                 </div>
-                                <!-- {{-- <div class="relative">
-                                    <input autocomplete="off" id="password" name="password" type="password"
-                                        class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                                        placeholder="Password" />
-                                    <label for="password"
-                                        class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-                                </div> --}} -->
                                 <div class="relative">
                                     <button class="bg-violet-500 text-white rounded-md px-2 py-1">Submit</button>
                                 </div>
